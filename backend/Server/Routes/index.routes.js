68218b17f7f4a926654e7f7a4a1076156d373b07
YeffersonParra01 <londonojefferson04@gmail.com>
2025-02-21 07:49:33 -0500

    Se modifico usuariosRutas.js

--- backend/Server/Routes/index.routes.js
+++ backend/Server/Routes/index.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const indexRoutes = express.Router();
 
-const aprendicesRoutes = require('./aprendicesRutas.js');
+// const aprendicesRoutes = require('./aprendicesRutas.js');
 const reportes = require('./reporteRutas.js');
+const usuarios = require('./usuariosRutas.js');
 
-indexRoutes.use('/aprendices', aprendicesRoutes);
+// indexRoutes.use('/aprendices', aprendicesRoutes);
 indexRoutes.use('/reportes', reportes);
+indexRoutes.use('/usuarios', usuarios);
 
 module.exports = indexRoutes;
