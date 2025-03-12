bd3e4d64a01b2db7c1a00f19a31d7880c024284a
lmejiamartinez <lmejiamartinez25@gmail.com>
2025-03-12 13:28:53 -0500

    Agregando Bitacoras

--- backend/Server/Routes/index.routes.js
+++ backend/Server/Routes/index.routes.js
@@ -4,11 +4,12 @@ const indexRoutes = express.Router();
 // const aprendicesRoutes = require('./aprendicesRutas.js');
 const Reporte = require('./Reporte.js');
 const usuarios = require('./usuariosRutas.js');
-const certificados = require('./certificadosRutas.js')
-
+const certificados = require('./certificadosRutas.js');
+const bitacoras = require('./bitacoraRutas.js');
 // indexRoutes.use('/aprendices', aprendicesRoutes);
 indexRoutes.use('/reportes', Reporte);
 indexRoutes.use('/usuarios', usuarios);
 indexRoutes.use('/certificados', certificados);
+indexRoutes.use('/bitacoras',bitacoras);
 
 module.exports = indexRoutes;
