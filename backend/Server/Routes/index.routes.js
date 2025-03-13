c6c81b2ac5a61296430fd9e05e76e3d3fde1e59e
YeffersonParra01 <londonojefferson04@gmail.com>
2025-03-13 12:22:09 -0500

    Se realizaron adaptaciones para pruebas

--- backend/Server/Routes/index.routes.js
+++ backend/Server/Routes/index.routes.js
@@ -3,12 +3,14 @@ const indexRoutes = express.Router();
 
 // const aprendicesRoutes = require('./aprendicesRutas.js');
 const Reporte = require('./Reporte.js');
-const usuarios = require('./usuariosRutas.js');
-const certificados = require('./certificadosRutas.js')
+const usuarios = require('./Usuario.js');
+const certificados = require('./Certificado.js');
+const Visita = require('./Visita.js');
 
 // indexRoutes.use('/aprendices', aprendicesRoutes);
 indexRoutes.use('/reportes', Reporte);
 indexRoutes.use('/usuarios', usuarios);
 indexRoutes.use('/certificados', certificados);
+indexRoutes.use('/visitas', Visita);
 
 module.exports = indexRoutes;
