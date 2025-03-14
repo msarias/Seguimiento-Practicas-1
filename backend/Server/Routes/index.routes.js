edb35c493fc9864c58c5e8748f3aca249feea4c8
lmejiamartinez <lmejiamartinez25@gmail.com>
2025-03-13 19:25:47 -0500

    Avance en el modulo olvide mi contraseña

--- backend/Server/Routes/index.routes.js
+++ backend/Server/Routes/index.routes.js
@@ -6,11 +6,14 @@ const Reporte = require('./Reporte.js');
 const usuarios = require('./Usuario.js');
 const certificados = require('./Certificado.js');
 const Visita = require('./Visita.js');
+const Bitacora = require('./bitacoraRutas.js');
+
 
 // indexRoutes.use('/aprendices', aprendicesRoutes);
 indexRoutes.use('/reportes', Reporte);
 indexRoutes.use('/usuarios', usuarios);
 indexRoutes.use('/certificados', certificados);
+indexRoutes.use('/bitacoras',Bitacora);
 indexRoutes.use('/visitas', Visita);
 
 module.exports = indexRoutes;
