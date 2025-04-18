7b12da35f343bb772655c2e39a20294be1b6ba32
Michael Arias <maicolarias27@gmail.com>
2025-04-18 13:24:27 -0500

    Implement axios (bitacora)

--- src/components/bitacoras/BitacoraList.jsx
+++ src/components/bitacoras/BitacoraList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import BitacoraForm from './BitacoraForm';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BitacoraForm from "./BitacoraForm";
 
 const BitacoraList = () => {
   const [bitacoras, SetBitacoras] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   useEffect(() => {
     obtenerBitacoras();
@@ -11,68 +12,71 @@ const BitacoraList = () => {
 
   const obtenerBitacoras = async () => {
     try {
-      const res = await fetch(
-        'http://localhost:3000/api/bitacoras/verBitacoras'
-      );
-      const data = await res.json();
-      if (!res.ok) {
-        setError('No se pudieron obtener las bitácoras');
+      const url = "http://localhost:3000/api/bitacoras/verBitacoras";
+      const res = await axios.get(url);
+
+      const data = res.data;
+
+      if (!res.status === 200) {
+        setError("No se pudieron obtener las bitácoras.");
+      } else {
+        console.log(data);
+        SetBitacoras(data);
       }
-      console.log('data:', { data });
-      SetBitacoras(data);
-    } catch (error) {}
+    } catch (error) {
+      setError("Ocurrió un error:", error.message);
+    }
   };
-  const deleteBitacora = async (e) => {
-    const id = e.target.id;
-    try {
-      const res = await fetch(`http://localhost:3000/api/bitacoras/${id}`, {
-        method: 'DELETE',
-      });
+};
 
-      if (!res.ok) {
-        throw new Error('No se pudo eliminar la bitácora.');
-      }
+const deleteBitacora = async (e) => {
+  const id = e.target.id;
 
-      const data = await res.json();
-      console.log('Bitácora eliminada:', data);
+  try {
+    const url = `http://localhost:3000/api/bitacoras/${id}`;
+    const res = await axios.delete(url);
+
+    if (res.status === 200) {
+      console.log("Bitácora eliminada:", res.data);
+
       const updatedBitacoras = bitacoras.filter(
         (bitacora) => bitacora.id !== id
       );
-
       SetBitacoras(updatedBitacoras);
-    } catch (error) {
-      setError(error.message);
+    } else {
+      setError("No se pudo eliminar la bitácora");
     }
-  };
-
-  return (
-    <section className="bitacora-list">
-      <h2 className="bitacora-list__title">Bitácoras</h2>
-      {error && <p className="error-message">{error}</p>}
-      {bitacoras.length > 0 ? (
-        bitacoras.map((b, index) => (
-          <div className="report-list__item" key={index}>
-            <p>
-              <strong>{`Bitácora ${index + 1}`}</strong>
-            </p>
-            <p>{b.archivo || 'Sin archivo'}</p>
-            <p>{b.codigo}</p>
-            <p>{b.fecha}</p>
-            <button
-              id={b.id}
-              className="bitacora-list__button delete-button"
-              onClick={deleteBitacora}
-            >
-              <img id="delete-img" src="../css/img/trash.png" alt="Eliminar" />
-            </button>
-          </div>
-        ))
-      ) : (
-        <p>No hay bitácoras</p>
-      )}
-      <BitacoraForm />
-    </section>
-  );
+  } catch (error) {
+    console.error("Error al configurar la solicitud:", error.message);
+    setError("Ocurrió un error al eliminar la bitácora.");
+  }
 };
+
+return (
+  <section className="bitacora-list">
+    <h2 className="bitacora-list__title">Bitácoras</h2>
+    {error && <p className="error-message">{error}</p>}
+    {bitacoras.length > 0 ? (
+      bitacoras.map((b, index) => (
+        <div className="report-list__item" key={index}>
+          <b>{`Bitácora ${index + 1}`}</b>
+          <p>{b.archivo || "Sin archivo"}</p>
+          <p>{b.codigo || "Sin código"}</p>
+          <p>{b.fecha || "Sin fecha"}</p>
+          <button
+            id={b.id}
+            className="bitacora-list__button delete-button"
+            onClick={deleteBitacora}
+          >
+            <img id="delete-img" src="../css/img/trash.png" alt="Eliminar" />
+          </button>
+        </div>
+      ))
+    ) : (
+      <p>No hay bitácoras</p>
+    )}
+    <BitacoraForm />
+  </section>
+);
 
 export default BitacoraList;
