3aee57fecf2bba89ad3de628dfc97c3d2e6d5d66
RodrigoSerranoVillegas <rodrigoserranov40@gmail.com>
2025-05-07 11:15:54 -0500

    Imagen de header

--- src/components/login/Header.jsx
+++ src/components/login/Header.jsx
@@ -1,16 +1,16 @@
 const Header = () => {
   return (
     <header className="header">
-      <div className="logos-container">
+      <div className="logo-container">
         <img
-          src="../css/img/sena-logo-verde.png"
+          src="../assets/img/sena-logo-verde.png"
           alt="SENA Logo"
           id="logo-sena"
           draggable="false"
         />
       </div>
       <div className="title-block">
-        <h1 id="title">Seguimiento Etapa Práctica</h1>
+        <h1 className="title">Seguimiento Etapa Práctica</h1>
       </div>
     </header>
   );
